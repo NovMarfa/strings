70849c508c6b35e1bec6884d52da2bf62d20e9ac
NovMarfa <mn211195@gmail.com>
2024-11-24 21:54:15 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,13 @@
+example0 = 'Рождество'
+print (example0 [0])
+print (example0 [-1])
+print (example0 [4:])
+print (example0 [::-1])
+print (example0 [1:8:2])
+
+example = 'Топинамбур'
+print (example [0])
+print (example [-1])
+print (example [5:])
+print (example [::-1])
+print (example [1:10:2])
